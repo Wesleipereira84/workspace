3b00510b05d40577bbd8c678729e093dffd6602d
WESLEI JOSE PEREIRA <84477076+Wesleipereira84@users.noreply.github.com>
2021-06-10 19:49:31 -0300

    Add files via upload

---
+++ Atividade02/src/TorreHanoi03Disco.java
@@ -0,0 +1,15 @@
+
+public class TorreHanoi03Disco{
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Disco rosa torre 3");
+				System.out.println ("Disco lilas torre 2");
+				System.out.println ("Disco rosa torre 2");
+				System.out.println ("Disco roxo torre 3");
+				System.out.println ("Disco rosa torre 1");
+				System.out.println ("Disco lilas torre 3");
+				System.out.println ("Disco rosa torre 3");
+	}
+
+}
---
+++ Atividade02/src/TorreHanoi04Discos.java
@@ -0,0 +1,22 @@
+public class TorreHanoi04Discos{
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Disco rosa torre 2");
+				System.out.println("Disco lilas torre 3");
+				System.out.println("Disco rosa torre 3");
+				System.out.println("Disco roxo torre 2");
+				System.out.println("Disco rosa torre 1");
+				System.out.println("Disco lilas torre 2");
+				System.out.println("Disco rose torre 2");
+				System.out.println("Disco Azul torre 3");
+				System.out.println("Disco Rosa torre 3");
+				System.out.println("Disco Lilas torre 1");
+				System.out.println("Disco Rosa torre 1");
+				System.out.println("Disco Roxo torre 3");
+				System.out.println("Disco Rosa torre 2");
+				System.out.println("Disco lilas torre 3");
+				System.out.println("Disco Rosa torre 3");
+	}
+
+}
